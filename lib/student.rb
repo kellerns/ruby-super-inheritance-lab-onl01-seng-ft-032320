f0c2dfcfdf3e21f4b08bae85d6fe1562fb7df63f
Nicholas Keller <nic.keller328@gmail.com>
2020-04-09 00:17:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,11 @@ class Student
   end
 
   def hello
-    "Hey there! I'm so excited to learn stuff."
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    puts "Pick me!"
   end
 
 end
